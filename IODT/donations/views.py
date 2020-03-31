7d4c54905ed8bb798e7dd50acd5b63ec70599775
sahussawud <sahussawud@hotmail.com>
2020-03-31 23:04:06 +0700

    move out register donation to donations app
    -views
    -urls

--- IODT/donations/views.py
+++ IODT/donations/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+import donations
+
 # Create your views here.
+ def register_donations(request):
+    
+    return render(request, 'donations/registor_donation', {
+
+    }) 
