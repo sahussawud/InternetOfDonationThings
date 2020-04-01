f79f82c7c01b0470fbf71a26bf8478c7ab1448de
sahussawud <sahussawud@hotmail.com>
2020-04-01 11:38:41 +0700

    previews upload picture

--- IODT/donations/forms.py
+++ IODT/donations/forms.py
@@ -1,8 +1,16 @@
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, ImageField, widgets
 from donations.models import *
 from django.utils.translation import gettext_lazy as _
+from string import Template
+from django.utils.safestring import mark_safe
+
+class PictureWidget(widgets.Widget):
+    def render(self, name, value, attrs=None, **kwargs):
+        html =  Template("""<img src="$link"/>""")
+        return mark_safe(html.substitute(link=value))
 
 class DonationForm(ModelForm):
+    photo = ImageField(widget=PictureWidget)
     class Meta:
         model = Donation
         fields = ['name', '_type', 'condition', 'desc', 'quantity']
