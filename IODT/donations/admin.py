6f6cfe1ddf93a3311bd7cd3de31cbf0b85362ba4
sahussawud <sahussawud@hotmail.com>
2020-04-03 13:43:30 +0700

    fix a admin.py

--- IODT/donations/admin.py
+++ IODT/donations/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from register.models import user
+from register.models import user, Doner, Recipient, People, Organization
 
 admin.site.register(user, UserAdmin)
+admin.site.register(Doner)
+admin.site.register(Recipient)
+admin.site.register(People)
+admin.site.register(Organization)
