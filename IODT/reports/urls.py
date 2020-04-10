2d44a0b3bff045c19f58eb3d0656fd83da2e1fbe
sahussawud <sahussawud@hotmail.com>
2020-04-10 14:30:04 +0700

    complete project_view

--- IODT/reports/urls.py
+++ IODT/reports/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('project/<int:project_id>/', views.project_view, name='project_view'),
     # path('login/', views.login, name='login'),
 ]
--- IODT/reports/views.py
+++ IODT/reports/views.py
@@ -3,7 +3,29 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required, permission_required
 
+from donations.models import Project, Donation, Album, Picture
+
 # Create your views here.
 
-def project_view(request):
-    return render(request, 'report/project_view.html')
+def dashboard(request):
+    return render(request, 'report/report.html')
+
+def search(request):
+    return HttpResponse('Attendance Search Page.')
+
+def index(request):
+    return render(request, 'report/index.html',{})
+
+def project_view(request, project_id):
+    contexts = {}
+    project = Project.objects.get(pk=project_id)
+    project_picture = Picture.objects.filter(album=project.album)
+    donation_in_project = Donation.objects.filter(project=project)
+    print(project)
+    print(project_picture)
+    print(donation_in_project)
+
+    contexts['project'] = project
+    contexts['project_picture'] = project_picture
+    contexts['project_in_project'] = donation_in_project
+    return render(request, 'report/project_view.html',context=contexts)
