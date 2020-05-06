44234944fd8f3e00869472591e671544c5245924
sahussawud <sahussawud@hotmail.com>
2020-05-06 22:01:52 +0700

    Update settings.py

--- IODT/attendance/settings.py
+++ IODT/attendance/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'iodt',
+        'NAME': 'iodt2',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
