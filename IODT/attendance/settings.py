5bb1ed9c71da5a4e48b0a0a881d3c2e971eec472
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-05-06 22:19:26 +0700

    update

--- IODT/attendance/settings.py
+++ IODT/attendance/settings.py
@@ -82,13 +82,17 @@ WSGI_APPLICATION = 'attendance.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'iodt',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '1234',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'iodt',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
